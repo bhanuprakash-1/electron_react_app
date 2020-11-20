79a542b202ffd710e9133dc4e4c8faa0308fc8b0
bhanuprakash-1 <chinthalapallybhanu@gmail.com>
2020-11-20 20:04:51 +0530

    Sample_MOM.txt now is successfully read from public folder in dev, yet to test in prod

--- public/electron.js
+++ public/electron.js
@@ -7,13 +7,63 @@ const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
 
+const fs = require('fs');
+const readline = require('readline');
+const { ipcMain } = require('electron');
+
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({
+      width: 900, 
+      height: 680,
+      webPreferences:{
+        nodeIntegration:true
+      }
+    });
+
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
+  
+//   mainWindow.webContents.openDevTools()
 }
+
+const processLineByLine = async (text_field1,text_field2) =>{
+    sample_text_file_path = (isDev ? './public/Sample_MOM.txt' : `file://${path.join(__dirname, '../build/Sample_MOM.txt')}`);
+    
+    const fileStream = fs.createReadStream(sample_text_file_path);
+    
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+  
+    lines = ""
+    count = 0;
+    for await (line of rl){
+        count+=1;
+        if(count==40)
+        {
+            if(line.charAt(13)=='{')
+            {
+                console.log(" Got the { in 40 th line");
+                line = line.substring(0,14) + text_field1 + line.substring(14);
+            }
+        }
+  
+        lines+= line+'\n';
+        
+        
+    }
+  
+    return lines;
+};
+
+ipcMain.on('sample_mom',(event,args)=>{
+    processLineByLine("Cool","Cool2").then(lines =>{
+        event.sender.send('sample_mom_data',lines);
+    });
+})
 
 app.on('ready', createWindow);
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const electron = window.require('electron');
+const ipcRenderer = electron.ipcRenderer;
+
+class App extends Component {
+
+  state = {
+    text:""
+  }
+
+  componentDidMount(){
+    ipcRenderer.send('sample_mom');
+
+    ipcRenderer.on('sample_mom_data',(event,args)=>{
+      this.setState({text: args});
+    })
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>
+            {this.state.text};
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
